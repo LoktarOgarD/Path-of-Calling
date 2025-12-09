af86c3fc17d25dd13ef8eedc2fa33af44e3b52ec
LoktarOgarD <162473026+LoktarOgarD@users.noreply.github.com>
2025-12-09 10:06:35 +0100

    Update Player class structure and initialization
    Refactor Player class to initialize Stats in constructor

--- Path of Calling/Domain/Player.cs
+++ Path of Calling/Domain/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace PathOfCalling.Domain
@@ -5,21 +6,27 @@ namespace PathOfCalling.Domain
     public class Player
     {
         public string Name { get; set; } = "Wanderer";
-        public string ArchetypeId { get; set; } = "Knight";
+
+        // wird nach dem Test gesetzt: "Knight" / "Samurai" / "Viking" / "Bard"
+        public string ArchetypeId { get; set; } = "";
+
         public int Level { get; set; } = 1;
 
-        // 10 Punkte Basis, später +5 durch Level
-        public Dictionary<StatType, int> Stats { get; set; } =
-            new Dictionary<StatType, int>
-            {
-                { StatType.Strength, 0 },
-                { StatType.Discipline, 0 },
-                { StatType.Courage, 0 },
-                { StatType.Wisdom, 0 },
-                { StatType.Creativity, 0 }
-            };
+        // 5 Kern-Stats (Strength, Discipline, Courage, Wisdom, Creativity)
+        public Dictionary<StatType, int> Stats { get; set; }
 
+        // Ultimate-Status
         public bool UltimateUnlocked { get; set; } = false;
         public string UltimateName { get; set; } = "";
+
+        public Player()
+        {
+            // Alle Stats sauber auf 0 initialisieren
+            Stats = new Dictionary<StatType, int>();
+            foreach (StatType stat in (StatType[])Enum.GetValues(typeof(StatType)))
+            {
+                Stats[stat] = 0;
+            }
+        }
     }
 }
