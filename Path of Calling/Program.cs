d4571094d86c577ba551ac0cb0d9388859a6b484
LoktarOgarD <162473026+LoktarOgarD@users.noreply.github.com>
2025-12-09 10:34:16 +0100

    Set console output encoding to UTF-8
    Added UTF-8 output encoding for console.

--- Path of Calling/Program.cs
+++ Path of Calling/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using PathOfCalling.Domain;
 
 namespace PathOfCalling
 {
@@ -7,6 +8,8 @@ namespace PathOfCalling
         static void Main(string[] args)
         {
             Console.Title = "Path of Calling";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             var game = new Game();
             game.Run();
         }
